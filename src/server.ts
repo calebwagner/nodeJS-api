8b99d11a6af2f7724270195d10b826e82d68dcac
Caleb <calebwagner5@gmail.com>
2023-06-23 13:45:09 -0500

    added auth to app

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
-import express from 'express';
-import router from './router';
-import morgan from 'morgan';
+import express from "express";
+import router from "./router";
+import morgan from "morgan";
+import { protect } from "./modules/auth";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    console.log('GET request received');
-    res.status(200)
-    res.json({ message: 'Hello World!' })
-})
+app.get("/", (req, res) => {
+  console.log("GET request received");
+  res.status(200);
+  res.json({ message: "Hello World!" });
+});
 
-app.use('/api', router);
+app.use("/api", protect, router);
 
 export default app;
