814ae87e7d8cfbfe9bce7771d4756f48e1360bd4
Caleb <calebwagner5@gmail.com>
2023-06-23 15:48:10 -0500

    added basic auth

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,13 @@
 import jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
 
 export const createJWT = (user) => {
   const token = jwt.sign(
@@ -8,12 +17,32 @@ export const createJWT = (user) => {
   return token;
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
     res.status(401);
     res.json({ message: "Not authorized" });
+    return;
+  }
+
+  //   bearer: token
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    res.status(401);
+    res.json({ message: "Not authorized" });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    console.log(payload);
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.json({ message: "Not a valid token" });
     return;
   }
 };
